7c0fc47eea0687197293e5f46ec032eed6a03133
Lamija Mezit <80250132+LamijaMezit@users.noreply.github.com>
2023-02-03 20:06:19 +0100

    Data test

---
+++ Data-test/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Data_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+        [TestClass]
+        public class UnitTest
+        {
+
+            [DynamicData(nameof(ReadExcel), DynamicDataSourceType.Method)]
+            [TestMethod]
+
+            public void DataTesting(string name)
+            {
+                IWebDriver webDriver = new ChromeDriver();
+                webDriver.Manage().Window.Maximize();
+                webDriver.Url = "https://www.udemy.com/?deal_code=&utm_term=Homepage&utm_content=Textlink&utm_campaign=Rakuten-default&ranMID=39197&ranEAID=%2F68Yt01SgtI&ranSiteID=_68Yt01SgtI-_lQDkxguh3Ck8ukBDqWF4g&utm_source=aff-campaign&LSNPUBID=%2F68Yt01SgtI&utm_medium=udemyads";
+
+                webDriver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[3]/div[2]/form/input[2]")).SendKeys(name);
+                webDriver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[3]/div[2]/form/button")).Click();
+
+                webDriver.Quit();
+
+
+
+
+
+            }
+        }
+    
+
+
+
+
+}
---
+++ Data-test/ReadExcel.cs
@@ -0,0 +1,29 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Data_test
+{
+     public class ReadExcel
+    {
+        public static IEnumerable<object[]> RExcel()
+        {
+            using (ExcelPackage package = new ExcelPackage(new FileInfo("C:/Users/lamij/OneDrive/Desktop/Lamija1-data.xslx")))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["List"];
+                int rowCount = worksheet.Dimension.End.Row;
+                for ( int row=2; row<=rowCount; row++)
+                {
+                    yield return new object[]
+                    {
+                        worksheet.Cells[row,1].Value?.ToString().Trim()
+
+                    };
+                }
+
+            }
+        }
+    }
+}
